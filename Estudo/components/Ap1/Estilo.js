3cb518454e8cc09e5c2fef97fea970d66eecda7e
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2023-10-05 15:26:28 -0300

    navigation

--- Estudo/components/Ap1/Estilo.js
+++ Estudo/components/Ap1/Estilo.js
@@ -1,33 +1,42 @@
 import { StyleSheet } from "react-native";
 
-
 const estilos = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems: "center",
-        backgroundColor: "black",
-    },
-    header:{
-        fontWeight: "bold",
-        fontSize:15,
-        color: "white",
-    },
-    icon:{
-        fontWeight: "bold",
-        fontSize:15,
-        color: "white",
-        padding: 10,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "white",
+  },
+  icon: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "white",
+    padding: 10,
+  },
+  dados: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "white",
+    padding: 10,
+  },
+  title: {
+    padding: 10,
+  },
+  containerDados: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-evenly",
+    padding: 10,
+  },
+  preço:{
+    marginLeft:100,
+  }
+});
 
-    },
-    dados:{
-        fontWeight: "bold",
-        fontSize:15,
-        color: "white",
-        padding: 10,
-
-    }
-
-})
-
-export default estilos
+export default estilos;
--- Estudo/components/Ap1/Questao01App.jsx
+++ Estudo/components/Ap1/Questao01App.jsx
@@ -2,19 +2,19 @@ import { View, SectionList, Text } from "react-native";
 import dadosCompra from "./Dados";
 import estilos from "./Estilo";
 
-const Questao01App = () => {
+const Questao01App = ({navigation}) => {
   return (
     <View style={estilos.container}>
       <SectionList
         sections={dadosCompra}
         keyExtractor={(item, index) => item + index}
         renderSectionHeader={({ section: { title } }) => (
-          <View style={{ padding: 10 }}>
+          <View style={estilos.title}>
             <Text style={estilos.header}>{title}</Text>
           </View>
         )}
         renderItem={({ item }) => (
-          <View style={{ flex:1,flexDirection: "row", alignItems: "flex-start", justifyContent: "space-evenly", padding: 10 }}>
+          <View style={estilos.containerDados}>
             <View>
               <Text style={estilos.icon}>{item.icon}</Text>
             </View>
@@ -22,7 +22,7 @@ const Questao01App = () => {
               <Text style={estilos.dados}>{item.nome}</Text>
               <Text style={estilos.dados}>{item.horario}</Text>
             </View>
-            <View style={{marginLeft:100}} >
+            <View style={estilos.preço} >
               <Text style={estilos.dados}>{item.valor}</Text>
             </View>
           </View>
